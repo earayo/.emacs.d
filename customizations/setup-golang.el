537afbf1de84ef6d938dcd10d84dfef0370f0596
edwaraco <edward.alejandro.cortes@gmail.com>
2020-04-20 12:25:26 -0500

    Added hooks or short cuts for golang

--- customizations/setup-golang.el
+++ customizations/setup-golang.el
@@ -7,3 +7,41 @@
 (require 'go-autocomplete)
 (require 'auto-complete-config)
 (require 'go-errcheck)
+
+
+(add-hook 'before-save-hook 'gofmt-before-save)
+
+(add-hook
+ 'go-mode-hook (lambda () (setq indent-tabs-mode t)))
+
+(add-hook 'go-mode-hook (lambda ()
+                          (local-set-key (kbd "C-c C-r") 'go-remove-unused-imports)))
+
+(add-hook 'go-mode-hook (lambda ()
+                          (local-set-key (kbd "C-c i") 'go-goto-imports)))
+
+(add-hook 'go-mode-hook (lambda ()
+                          (local-set-key (kbd "C-c m") 'go-errcheck)))
+
+(add-hook 'go-mode-hook (lambda ()
+                          (local-set-key (kbd "C-c C-r") 'go-remove-unused-imports)))
+
+(add-hook 'go-mode-hook (lambda ()
+                          (local-set-key (kbd "C-c d") 'godoc-at-point)))
+
+(defun auto-complete-for-go ()
+  (auto-complete-mode 1))
+
+(add-hook 'go-mode-hook 'auto-complete-for-go)
+
+(defun set-go-path ()
+  "Set GOPATH and GOBIN"
+  (interactive)
+  (let (gopath)
+    (setq gopath (read-from-minibuffer "GOPATH: " (file-name-directory (or load-file-name buffer-file-name))))
+    (message (concat "Setting GOPATH to " gopath))
+    (setenv "GOPATH" gopath)
+    (setenv "GOBIN" (concat gopath "/bin"))))
+
+(add-hook 'go-mode-hook (lambda ()
+                          (local-set-key (kbd "C-c p") 'set-go-path)))
