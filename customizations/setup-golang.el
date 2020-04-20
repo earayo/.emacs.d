06e60ea8561b3688ecde0642ae748a713fe961ea
edwaraco <edward.alejandro.cortes@gmail.com>
2020-04-20 10:42:35 -0500

    Added gocode lib for emacs

--- customizations/setup-golang.el
+++ customizations/setup-golang.el
@@ -1,7 +1,9 @@
 ;; Imports libs
 (add-lib-path "go-mode")
 (add-lib-path "go-errcheck")
-
+(add-lib-path "gocode/emacs")
 
 (require 'go-mode)
+(require 'go-autocomplete)
+(require 'auto-complete-config)
 (require 'go-errcheck)
